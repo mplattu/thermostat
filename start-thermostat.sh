02951fd2d9b95651cbfa4dfb692e3f96013c7dac
Matti Lattu <matti.lattu@iki.fi>
2020-03-09 07:43:37 +0200

    Write script start message to debug log only

--- start-thermostat.sh
+++ start-thermostat.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+LOG=/var/log/thermostat.log
+
 # Create 1-wire filesystem
 mkdir /tmp/owfs
 owfs -u -m /tmp/owfs
@@ -10,7 +12,8 @@ cd /srv/thermostat/thermostat
 # Loop forever
 while [ 1 != 2 ]
 do
-  ./thermostat.py --owfs /tmp/owfs 2>>/var/log/thermostat.log
-  echo "start-thermostat.sh executed"
+  echo "--- now executing start-thermostat.sh" >>$LOG
+  ./thermostat.py --owfs /tmp/owfs 2>>$LOG
+  echo "--- executed start-thermostat.sh" >>$LOG
   sleep $THERMOSTAT_DELAY_BETWEEN
 done
