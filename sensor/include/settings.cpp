aec17e78302beb25f9ab5c68dd5f5a43b8886b87
Matti Lattu <matti.lattu@iki.fi>
2021-11-21 10:17:46 +0200

    Initial version of Wemos D1 sensor code

---
+++ sensor/include/settings.cpp
@@ -0,0 +1,6 @@
+// SENSOR_NAME will be broadcasted along the temperature
+const char *SENSOR_NAME = "Sensor 2";
+
+// WiFi name and key
+const char *WIFI_SSID = "vilpola";
+const char *WIFI_PASS = "ragorago";
